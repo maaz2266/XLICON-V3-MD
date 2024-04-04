2c83d5ce37b1d7b8c8fac80b78dbe998e751787a
maaz2266 <156607236+maaz2266@users.noreply.github.com>
2024-04-04 12:33:33 +0500

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@
 //Instagram: unicorn_xeon13
 //Telegram: t.me/ahmmitech
 //GitHub: @salmanytofficial
-//WhatsApp: +923184070915
+//WhatsApp: 33644624722
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
 //   * Created By Github: DGXeon.
 //   * Credit To Xeon
